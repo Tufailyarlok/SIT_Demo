90027bcee8d1e779f2763e18a7178f95834f5d7c
Tufailyarlok <144475407+Tufailyarlok@users.noreply.github.com>
2025-01-21 02:18:58 +0530

    this is first commit.

---
+++ wp-content/themes/lms-education-university/core/sections/slider.php
@@ -0,0 +1,52 @@
+<?php if ( get_theme_mod('lms_education_blog_box_enable',false) ) : ?>
+
+<?php $args = array(
+  'post_type' => 'post',
+  'post_status' => 'publish',
+  'category_name' =>  get_theme_mod('lms_education_blog_slide_category'),
+  'posts_per_page' => get_theme_mod('lms_education_blog_slide_number'),
+); ?>
+
+<div class="slider">
+  <div class="owl-carousel">
+    <?php $lms_education_university_arr_posts = new WP_Query( $args );
+    if ( $lms_education_university_arr_posts->have_posts() ) :
+      while ( $lms_education_university_arr_posts->have_posts() ) :
+        $lms_education_university_arr_posts->the_post();
+        ?>
+        <div class="blog_box">
+          <?php
+            if ( has_post_thumbnail() ) :
+              the_post_thumbnail();
+            else:
+              ?>
+              <div class="slider-alternate">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/banner.png'; ?>">
+              </div>
+              <?php
+            endif;
+          ?>
+          <div class="content_inner_box wow zoomIn">
+            <hr>
+            <?php if ( get_theme_mod('lms_education_slider_text_extra') ) : ?>
+                <h5><?php echo esc_html(get_theme_mod('lms_education_slider_text_extra'));?></h5>
+              <?php endif; ?>
+            <?php if ( get_theme_mod('lms_education_title_unable_disable',true) ) : ?>
+            <h3 class="post-title mb-3 mt-0"><a href="<?php echo esc_url(get_permalink($post->ID)); ?>"><?php the_title(); ?></a></h3>
+            <?php endif; ?>
+            <?php if ( get_theme_mod('lms_education_text_unable_disable',true) ) : ?>
+            <p><?php echo wp_trim_words( get_the_content(), get_theme_mod('lms_education_slide_excerpt_number',20) ); ?></p>
+            <?php endif; ?>
+            <div class="slider-btn">
+              <a href="<?php the_permalink(); ?>"><?php esc_html_e('Enroll Now','lms-education-university'); ?></a>
+            </div>
+          </div>
+        </div>
+      <?php
+    endwhile;
+    wp_reset_postdata();
+    endif; ?>
+  </div>
+</div>
+
+<?php endif; ?>
